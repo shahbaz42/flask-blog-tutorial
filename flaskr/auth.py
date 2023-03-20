a1197fbe7f6cffb541bb8d3958b3094ef2040968
Shahbaz <shahbaz_ali@outlook.in>
2023-03-20 22:40:13 +0530

    add decorator to protect routes

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -88,3 +88,13 @@ def load_logged_in_user():
 def logout():
     session.clear()
     return redirect(url_for('index'))
+
+def login_required(view):
+    @functools.wraps(view)
+    # above line helps in preserving the metadata and docstring of the originam function.
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+        return view(**kwargs)
+    return wrapped_view
+
